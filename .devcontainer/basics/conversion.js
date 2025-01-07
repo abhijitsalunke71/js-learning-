bca1513de98b65d325dd996ffd21d20f142c84a4
Abhijit Subhash Salunke <126096743+abhijitsalunke71@users.noreply.github.com>
2025-01-07 08:06:09 +0000

    all about var,comp and conversion

---
+++ .devcontainer/basics/conversion.js
@@ -0,0 +1,36 @@
+let name = "abhijit";
+//console.log(name);
+//console.log(typeof(name));
+
+
+
+//converted into the number
+
+var convnumber = Number(name);
+//console.log(convnumber);
+//console.log(typeof(convnumber));
+
+//converted into the bolean value 
+
+var convboolean = Boolean(name);
+//console.log(convboolean); //==> true because "if there is any value it will return true if there is no any value it will return the false"
+
+
+
+//------ operations ------
+
+console.log(2+2);
+
+console.log("1"+2);
+console.log(2+"2");
+
+console.log("1"+ 2 + 2);
+console.log(1 + 4 + 5 + "1")// 
+
+let x=3;
+const y=x++
+console.log(x,y);
+
+let a =3;
+const b=++a;
+console.log(a,b);
---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,41 @@
+// const accountNumber= 1234;
+// var name="abhijit"
+// let address = "vadgaon rasai "
+
+
+// console.log(accountNumber);
+// console.log(address);
+
+
+// console.table([accountNumber,name,address])
+
+// let newname = Number(name);
+// console.log(newname)
+// var printing = typeof(newname);
+// console.log(printing);
+
+
+
+let name = "abhijit";
+var age = 23;
+
+console.log(typeof(name));
+console.log(typeof(age));
+
+let ISPresent = true;
+console.log(typeof ISPresent);
+
+
+const RoomTemp=null;
+console.log(typeof(RoomTemp));
+
+let character = undefined;
+console.log(typeof(character));
+
+const classing = Symbol('123');
+console.log(typeof(classing));
+
+var stockmarket = 123456789132n;
+console.log(typeof(stockmarket));
+
+
