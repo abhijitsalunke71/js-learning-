297b2b5fdc6b279901e0165e07c12e73c812fc6a
Abhijit Subhash Salunke <126096743+abhijitsalunke71@users.noreply.github.com>
2025-01-09 04:43:26 +0000

    learn about the API's

--- .devcontainer/basics2/object.js
+++ .devcontainer/basics2/object.js
@@ -45,3 +45,24 @@ reguser = {
 }
 
 console.log(reguser.fullname);
+
+
+
+
+//Object Destructuring 
+
+const empinfo ={
+        empname : "xyz",
+        empsalary : "15000",
+        emprole : "developer"
+}
+
+const {empname : emp,emprole : empr} = empinfo;
+console.log(emp,empr);
+
+
+
+//API takes value from the backend and gives that value in the XML format but the XML was not properly readable so now 
+//days they are using JSON format to show the api info ..
+//the value should be in the form of Object or in Array as Well.
+//but in the mostly cases the values in the objects. 
